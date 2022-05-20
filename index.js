9657cee9ae9bf48f6f5d7ccc34e9b17ca9c4f7f7
SepehrAshs <sepehrashs@gmail.com>
2022-05-20 23:33:32 +0430

    Authenticate Token Implemented

--- index.js
+++ index.js
@@ -121,3 +121,18 @@ async function signIn(username, userpass) {
   }
   
 }
+
+function authenticateToken(req, res, next){
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null)
+      return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) =>{
+      if (err)
+          return res.sendStatus(403);
+      req.user = user;
+      console.log("authenticated successfuly")
+      next(); 
+  })
+
+}
